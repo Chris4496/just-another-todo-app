3dbfb19e8d5608b1c8ff340c521ba04128fc6cad
Chris4496 <62294842+Chris4496@users.noreply.github.com>
2022-03-04 12:32:53 +0800

    Added localstorage api

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import List from "./component/List";
 import AddItem from "./component/AddItem";
 import Modal from "./component/Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ItemProps } from "./interfaces";
 
 function App() {
-  const [list, setList] = useState<ItemProps[]>([]);
+  const [list, setList] = useState<ItemProps[]>(() => {
+    const saved = localStorage.getItem("list");
+    const initialValue = JSON.parse(saved!);
+    return initialValue || [];
+  });
+
   // const [list, setList] = useState<ItemProps[]>([
   //   {
   //     id: 1,
@@ -26,6 +31,10 @@ function App() {
   //       "Contrary to popular belief, Lorem Ipsum is not simply random text.",
   //   },
   // ]);
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   const addItem = (item: ItemProps) => {
     setList([...list, item]);
@@ -49,7 +58,7 @@ function App() {
 
   return (
     <div className="bg-[url('https://picsum.photos/1920/1080')] min-h-screen flex flex-col flex-end content-start">
-      <div className="relavtive m-auto backdrop-blur-sm backdrop-opacity-90 drop-shadow-lg bg-white/30 rounded-md py-5 px-10 ">
+      <div className="relavtive m-auto backdrop-blur-sm backdrop-opacity-90 drop-shadow-lg bg-white/30 rounded-md py-5 px-10">
         <List itemList={list} removeItem={removeItem} />
         <div className="flex justify-center items-center mt-2">
           <AddItem toggleModal={toggleModal} />
